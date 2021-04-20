b79e8158074b464644092890c2524b13e45e7a0c
Matheus Gomes <matheusgomes192@hotmail.com>
2021-04-20 20:46:54 -0300

    Iniciando projeto com tudo configurado + arquivos em TS e SCSS + Components Header e Sidebar + atribuindo variaveis SCSS para uso global, para isso importa-se o arquivo no _app.tsx

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,8 @@
+import styles from './styles.module.scss';
+
+export function Sidebar() {
+    return(
+        <div className={styles.sidebarContainer}>
+        </div>
+    );
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import { Header }  from '../components/Header';
+import { Sidebar } from '../components/Sidebar';
+ 
+import '../styles/globals.scss'
+import styles from '../styles/app.module.scss';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div className={styles.wrapper}>
+      <Sidebar />
+
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
+}
+
+export default MyApp
