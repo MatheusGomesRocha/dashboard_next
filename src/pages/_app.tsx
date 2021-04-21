6d90bc0f06a812db6af5760f176a711c7215ad9b
Matheus Gomes <matheusgomes192@hotmail.com>
2021-04-20 21:41:50 -0300

    Adicionando REMOTE do git

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { Header }  from '../components/Header';
 import { Sidebar } from '../components/Sidebar';
- 
 import '../styles/globals.scss'
 import styles from '../styles/app.module.scss';
 
