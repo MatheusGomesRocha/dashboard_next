a54e7f2a665e4c622b277edc99c18cb42b41bde5
Matheus Gomes <matheusgomes192@hotmail.com>
2021-04-21 20:28:37 -0300

    Criando Services/api para criar o caminho padrao da URL da Api + retornando com sucesso os usuários na tela Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,59 @@
+import { GetStaticProps } from "next";
+import { api } from "../services/api";
 
-export default function Home() {
+type User = {
+  id: BigInteger,
+  name: string,
+  salary: string,
+  country: string,
+  city: string
+}
+
+type UserProps = {
+  users: User[]
+}
+
+export default function Home({users}: UserProps) {
+
+  console.log(users);
   return (
     <div>
-      
+      {users.map(item => {
+        return(
+          <div>
+            <h2>{item.name}</h2>
+
+          </div>
+        )
+      })}
     </div>
   )
 }
+
+export const getStaticProps = async () => {
+  const response = await api.get('users');
+  const data = response.data;
+
+  console.log(data);
+
+  const users = data.map(user => {
+    return{
+      id: user.id,
+      name: user.name,
+      salary: user.salary,
+      country: user.country,
+      city: user.city
+    }
+  })
+
+
+  return {
+    props: {
+      users
+    },
+
+    revalidate: 60 * 60 * 8 // 8 HOURS
+  }
+  
+}
+
