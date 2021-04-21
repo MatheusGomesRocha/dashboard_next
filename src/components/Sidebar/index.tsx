5b1c9127ca1815d27808dbf5ee993aa8293356d6
Matheus Gomes <matheusgomes192@hotmail.com>
2021-04-20 23:41:50 -0300

    Usando useState para pegar em qual link do sidebar o usuário clicou, e mudando o background e color do item selecionado para diferenciar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,6 +1,11 @@
+import { useState } from 'react';
 import styles from './styles.module.scss';
 
 export function Sidebar() {
+    const [sidebarLink, setSidebarLink] = useState('Dashboard');
+
+    const array = ['Dashboard', 'User Profile', 'Table List', 'Typography', 'Icon', 'Maps', 'Notifications'];
+
     return(
         <div className={styles.sidebarContainer}>
             <div className={styles.backgroundTransparent} />
@@ -10,13 +15,12 @@ export function Sidebar() {
             </div>
 
             <ul>
-                <li>Dashboard</li>
-                <li>User Profile</li>
-                <li>Table List</li>
-                <li>Typography</li>
-                <li>Icons</li>
-                <li>Maps</li>
-                <li>Notifications</li>
+                {array.map((item, k) => (
+                    <li style={{
+                        background: sidebarLink === item && '#e6e8eb', 
+                        color: sidebarLink === item && '#121212'
+                    }} onClick={() => setSidebarLink(item)}>{item}</li>
+                ))}
             </ul>
         </div>
     );
