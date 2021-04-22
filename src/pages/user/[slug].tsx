90769741c8749bdad4b50426a95881ccfcd86af3
Matheus Gomes <matheusgomes192@hotmail.com>
2021-04-21 21:57:47 -0300

    Adicionando inputs ao form na tela User Profile

--- src/pages/user/[slug].tsx
+++ src/pages/user/[slug].tsx
@@ -1,5 +1,6 @@
 import styles from './user.module.scss';
 import { useRouter } from 'next/router';
+import TextField from '@material-ui/core/TextField';
 
 export default function User() {
     const router = useRouter();
@@ -14,7 +15,27 @@ export default function User() {
                 </div>
 
                 <form>
-                    
+                    <div>
+                        <TextField label="Company (disabled)" disabled />
+                        <TextField label="Nome de usuário" />
+                        <TextField label="Email" />
+                    </div>
+
+                    <div>
+                        <TextField label="Primeiro Name" />
+                        <TextField label="Último Name" />
+                    </div>
+
+                    <div>
+                        <TextField label="Endereço" />
+                    </div>
+
+                    <div>
+                        <TextField label="Cidade" />
+                        <TextField label="País" />
+                        <TextField label="CEP" />
+                    </div>
+
                 </form>
             </div>
         </div>
