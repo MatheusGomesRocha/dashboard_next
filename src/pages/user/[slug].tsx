7b34303273936a81f05d03574252a0759cd620d3
Matheus Gomes <matheusgomes192@hotmail.com>
2021-04-24 11:11:22 -0300

    Fianlizando frontend da tela User

--- src/pages/user/[slug].tsx
+++ src/pages/user/[slug].tsx
@@ -15,6 +15,10 @@ type User = {
     city: string,
     salary: string,
     avatar: string,
+    friends: number,
+    photos: number,
+    comments: number,
+    age: number,
 };
 
 type UserProps = {
@@ -26,82 +30,123 @@ export default function User({ user }: UserProps) {
         <div className={styles.user}>
             <Path path="User Profile" />
 
-            <form>
-                <div className={styles.header}>
-                    <span>Editar Perfil</span>
-                </div>
+            <div className={styles.userInfo}>
+                <form>
+                    <div className={styles.header}>
+                        <span>Editar Perfil</span>
+                    </div>
 
-                <div className={styles.divInput}>
-                    <span>Informações do usuário</span>
+                    <div className={styles.divInput}>
+                        <span>Informações do usuário</span>
 
-                    <div className={styles.inputs}>
-                        <div>
-                            <label>Usuário</label>
-                            <input placeholder={user.name} />
+                        <div className={styles.inputs}>
+                            <div>
+                                <label>Usuário</label>
+                                <input placeholder={user.name} />
+                            </div>
+                            
+                            <div>
+                                <label>Email</label>
+                                <input placeholder={user.name} />
+                            </div>
+                            
+                            <div>
+                                <label>Primeiro Nome</label>
+                                <input placeholder={user.name} />
+                            </div>
+                            
+                            <div>
+                                <label>Último Nome</label>
+                                <input placeholder={user.name} />
+                            </div>
                         </div>
                         
-                        <div>
-                            <label>Email</label>
-                            <input placeholder={user.name} />
-                        </div>
-                        
-                        <div>
-                            <label>Primeiro Nome</label>
-                            <input placeholder={user.name} />
-                        </div>
-                        
-                        <div>
-                            <label>Último Nome</label>
-                            <input placeholder={user.name} />
+                    </div>
+                
+                    <div className={styles.divInput}>
+                        <span>Informações de Endereço</span>
+
+                        <div className={styles.inputs}>
+                            <div>
+                                <label>Endereço</label>
+                                <input placeholder={user.name} />
+                            </div>
+                            
+                            <div className={styles.secondChild}>    
+                                <div>
+                                    <label>Cidade</label>
+                                    <input placeholder={user.name} />
+                                </div>
+                                
+                                <div>
+                                    <label>País</label>
+                                    <input placeholder={user.name} />
+                                </div>
+                                
+                                <div>
+                                    <label>CEP</label>
+                                    <input placeholder={user.name} />
+                                </div>
+                            </div>
+
                         </div>
                     </div>
+
+                    <div className={styles.divInput}>
+                        <span>Informações de Endereço</span>
+
+                        <div className={styles.inputs}>
+                            <div>
+                                <label>Sobre mim</label>
+                                <textarea placeholder={user.name} />
+                            </div>
+                        </div>
+                    </div>
+                
+                    <div className={styles.divButton}>
+                        <button type="button">Editar</button>
+                    </div>
+                </form>
+
+                <section className={styles.profile}>
+                    <div className={styles.topBackground} />
+
+                    <section className={styles.img}>
+                        <Image
+                            src={user.avatar}
+                            width={500}
+                            height={500}
+                            objectFit="cover"
+                        />
+                    </section>
                     
-                </div>
-            
-                <div className={styles.divInput}>
-                    <span>Informações de Endereço</span>
-
-                    <div className={styles.inputs}>
+                    <section className={styles.userHistory}>
                         <div>
-                            <label>Endereço</label>
-                            <input placeholder={user.name} />
+                            <span>{user.friends}</span>
+                            <span>Amigos</span>
                         </div>
-                        
-                        <div className={styles.secondChild}>    
-                            <div>
-                                <label>Cidade</label>
-                                <input placeholder={user.name} />
-                            </div>
-                            
-                            <div>
-                                <label>País</label>
-                                <input placeholder={user.name} />
-                            </div>
-                            
-                            <div>
-                                <label>CEP</label>
-                                <input placeholder={user.name} />
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
-                <div className={styles.divInput}>
-                    <span>Informações de Endereço</span>
-
-                    <div className={styles.inputs}>
                         <div>
-                            <label>Sobre mim</label>
-                            <textarea placeholder={user.name} />
+                            <span>{user.photos}</span>
+                            <span>Fotos</span>
                         </div>
-                    </div>
-                </div>
+                        <div>
+                            <span>{user.comments}</span>
+                            <span>Comentários</span>
+                        </div>
+                    </section>
+
+                    <section className={styles.userPersonal}>
+                        <span>{user.name}, <span style={{color: '#afb2b1'}}>{user.age}</span></span>
+
+                        <span>{user.city}, {user.country}</span>
+
+                        <span>{user.role}</span>
+
+                        <p>{user.about}</p>
+                    </section>
+                </section>
+            </div>
             
-                <div className={styles.divButton}>
-                    <button type="button">Editar</button>
-                </div>
-            </form>
         </div>
     )
 }
@@ -128,7 +173,11 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
         avatar: data.avatar,
         country: data.country,
         city: data.city,
-        salary: data.salary
+        salary: data.salary,
+        age: data.age,
+        photos: data.photos,
+        friends: data.friends,
+        comments: data.comments
       }
 
     return {
